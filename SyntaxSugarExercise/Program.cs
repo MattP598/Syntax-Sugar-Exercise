37fe0a2e57e12554800410b2bd8257cef7013b2e
Matt <MattP598@gmail.com>
2022-04-26 10:43:15 -0500

    Syntax Sugar Exercise

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9) ? answer + " is less than nine" : answer + " is greater than or equal to nine";
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+                response = answer + " is greater than or equal to nine";
+            //};
+
+            Console.WriteLine(response);
+        }
+    }
+}
